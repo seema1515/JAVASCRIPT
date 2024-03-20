fb5b68014591a732d4fae3bed0b93eed2db24b94
Seema <seema.sharma.mlsm@gmail.com>
2024-03-20 12:41:25 -0400

    changes

--- first.js
+++ first.js
@@ -1,1 +1,14 @@
-console.log("apna college");
+let count = 6;
+
+while (count < 5) {
+  console.log("Count is: " + count);
+  count++;
+}
+
+console.log("+++++++++++++++++");
+let x = 6;
+
+do {
+  console.log("The value of x is: " + x);
+  x++;
+} while (x < 5);
